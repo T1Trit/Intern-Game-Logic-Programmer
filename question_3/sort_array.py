c326ca5952dc398865fe8028648c3e573217fe81
Bogdan Mekeda <466685@niuitmo.ru>
2025-01-15 11:22:44 +0300

    Create sort_array.py

---
+++ question_3/sort_array.py
@@ -0,0 +1,13 @@
+def sort_array_fastest(arr):
+  """
+  Сортирует массив чисел по месту, используя самый быстрый алгоритм, доступный в Python.
+
+  Args:
+    arr: Список чисел для сортировки.
+  """
+  arr.sort()
+
+# Пример использования
+my_array = [4, 2, 5, 1, 3]
+sort_array_fastest(my_array)
+print(my_array)  # Output: [1, 2, 3, 4, 5]
